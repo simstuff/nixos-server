35ba78c3922f6fe6953eae430808c88d2c0b1126
simstuff <82052330+simstuff@users.noreply.github.com>
2023-12-05 17:58:42 +0100

    adapt initial script

--- scripts/nix-setup.sh
+++ scripts/nix-setup.sh
@@ -1,53 +1,59 @@
 DISK=/dev/sda
+MAPPER=/dev/mapper/crypted
 
-parted "$DISK" -- mklabel gpt
-parted "$DISK" -- mkpart ESP fat32 1MiB 1GiB
-parted "$DISK" -- set 1 boot on
-mkfs.vfat "$DISK"1
+sudo parted "$DISK" -- mklabel gpt
+sudo parted "$DISK" -- mkpart ESP fat32 1MiB 512MiB
+sudo parted "$DISK" -- set 1 esp on
+sudo mkfs.fat -F 32 -n BOOT  "$DISK"1 
 
 # As I intend to use this VM on Proxmox, I will not encrypt the disk
 
-parted "$DISK" -- mkpart Swap linux-swap 1GiB 9GiB
-mkswap -L Swap "$DISK"2
-swapon "$DISK"2
+sudo parted "$DISK" -- mkpart Swap linux-swap 512MiB 4GiB
+sudo mkswap -L Swap "$DISK"2
+sudo swapon "$DISK"2
 
-parted "$DISK" -- mkpart primary 9GiB 100%
-mkfs.btrfs -L Butter "$DISK"3
+sudo parted "$DISK" -- mkpart primary 9GiB 100%
+# encrypt
+sudo cryptsetup luksFormat "$DISK"3
+sudo cryptedsetup luksConfig "$DISK"3 --label NIXOS
+sudo cryptsetup luksOpen "$DISK"3 crypted 
 
-mount "$DISK"3 /mnt
-btrfs subvolume create /mnt/root
-btrfs subvolume create /mnt/home
-btrfs subvolume create /mnt/nix
-btrfs subvolume create /mnt/persist
-btrfs subvolume create /mnt/log
+sudo mkfs.btrfs -L Butter "$MAPPER"
+
+sudo mount "$MAPPER" /mnt
+sudo btrfs subvolume create /mnt/root
+sudo btrfs subvolume create /mnt/home
+sudo btrfs subvolume create /mnt/nix
+sudo btrfs subvolume create /mnt/persist
+sudo btrfs subvolume create /mnt/log
 
 # We then take an empty *readonly* snapshot of the root subvolume,
 # which we'll eventually rollback to on every boot.
-btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
+sudo btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
 
-umount /mnt
+sudo umount /mnt
 
 # Mount the directories
 
-mount -o subvol=root,compress=zstd,noatime "$DISK"3 /mnt
+sudo mount -o subvol=root,compress=zstd,noatime "$Mapper" /mnt
 
-mkdir /mnt/home
-mount -o subvol=home,compress=zstd,noatime "$DISK"3 /mnt/home
+sudo mkdir /mnt/home
+sudo mount -o subvol=home,compress=zstd,noatime "$Mapper" /mnt/home
 
-mkdir /mnt/nix
-mount -o subvol=nix,compress=zstd,noatime "$DISK"3 /mnt/nix
+sudo mkdir /mnt/nix
+sudo mount -o subvol=nix,compress=zstd,noatime "$Mapper" /mnt/nix
 
-mkdir /mnt/persist
-mount -o subvol=persist,compress=zstd,noatime "$DISK"3 /mnt/persist
+sudo mkdir /mnt/persist
+sudo mount -o subvol=persist,compress=zstd,noatime "$Mapper" /mnt/persist
 
-mkdir -p /mnt/var/log
-mount -o subvol=log,compress=zstd,noatime "$DISK"3 /mnt/var/log
+sudo mkdir -p /mnt/var/log
+sudo mount -o subvol=log,compress=zstd,noatime "$Mapper" /mnt/var/log
 
 # don't forget this!
-mkdir /mnt/boot
-mount "$DISK"1 /mnt/boot
+sudo mkdir /mnt/boot
+sudo mount "$DISK"1 /mnt/boot
 
 # create configuration
-nixos-generate-config --root /mnt
+sudo nixos-generate-config --root /mnt
 
 # now, edit nixos configuration and nixos-install
